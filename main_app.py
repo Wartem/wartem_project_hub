60d25d2b5b35acb5b542c05d6115e99c97bcff7b
Wartem <mwasteson@gmail.com>
2024-08-02 17:52:57 +0200

    Minor string fixes
    Minor string fixes

--- main_app.py
+++ main_app.py
@@ -61,4 +61,4 @@ if __name__ == '__main__':
     for rule in app.url_map.iter_rules():
         print(f"{rule.endpoint}: {rule.rule}")
     # app.run(debug=True) For debugging
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
